9ed06056b68fa7336fe1e0d4c846ea254e85e94d
Aleksandr Rodionov <ravengothic.com>
2020-10-25 03:35:32 +0300

    i want to fucking sleep

--- src/main/java/TelegramBotApplication.kt
+++ src/main/java/TelegramBotApplication.kt
@@ -1,57 +1,78 @@
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
 import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow
+import java.io.File
 
 
-class TelegramBotApplication
+fun main() {
 
-fun main(args: Array<String>) {
     ApiContextInitializer.init()
     TelegramBotsApi().registerBot(Bot())
+
 }
 
 class Bot : TelegramLongPollingBot() {
+    private var parser: Parser = Parser.default()
+    private var stringBuilder: StringBuilder = StringBuilder(File("./package.json").readText())
+    private var json: JsonObject = parser.parse(stringBuilder) as JsonObject
+
     override fun getBotToken(): String {
         return "1357410487:AAG3jRz52vIIaARRdwr-4IShYqHCfTI1ZC8"
     }
 
     override fun onUpdateReceived(p0: Update?) {
-        if (p0!!.message.text == "/start") {
-            val keyboard = ReplyKeyboardMarkup()
-            val row = KeyboardRow()
-            keyboard.resizeKeyboard = true
 
-            row.add("Задать вопрос")
-            row.add("Попрощаться")
-
-            keyboard.keyboard.add(row)
-
+        when (p0!!.message.text) {
+            "/start" -> {
             execute(SendMessage()
                     .setChatId(p0.message.chatId)
-                    .setReplyMarkup(keyboard)
-                    .setText("Приветствие и объяснение функционала действий бота")
+                    .setText(json.string("greeting"))
+
             )
         }
-        else if (p0!!.message.text == "Задать вопрос") {
-            val keyboard = ReplyKeyboardMarkup()
-            val row = KeyboardRow()
-            row.add("Меню")
+        "Вернуться" ->
 
-            keyboard.keyboard.add(row)
-            keyboard.resizeKeyboard = true
-
-            val msg: SendMessage = SendMessage()
+            execute(SendMessage()
+                    .setReplyMarkup(ReplyKeyboardRemove())
                     .setChatId(p0.message.chatId)
-                    .setText("Задавайте вопрос")
-                    .setReplyMarkup(keyboard)
+                    .setText(json.string("comeback"))
+            )
 
-            execute(msg)
+        else ->
+            try {
+                val keyboard = ReplyKeyboardMarkup()
+
+                keyboard.keyboard = listOf(
+                        KeyboardRow().apply {
+                            add(KeyboardButton("Первый вопрос"))
+                            add(KeyboardButton("Второй вопрос"))
+                        },
+                        KeyboardRow().apply{
+                            add(KeyboardButton("Вернуться"))
+                        }
+                )
+
+                keyboard.resizeKeyboard = true
+
+                execute(SendMessage()
+                        .setReplyMarkup(keyboard)
+                        .setChatId(p0.message.chatId)
+                        .setText(json.string("ifelse")
+                                ?.replace("***", "\n1. Первый вопрос\n2. Второй вопрос\n")))
+            } catch (e: Exception) {
+                execute(SendMessage()
+                        .setChatId(p0.message.chatId)
+                        .setText(json.string("error")))
+            }
         }
-
     }
 
     override fun getBotUsername(): String {
